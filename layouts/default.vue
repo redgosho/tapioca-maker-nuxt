85da4f74c777ae7a67286d1084b0c3d7fe9f69d2
redgosho <redgosho@gmail.com>
2020-07-07 20:43:40 +0900

    first

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view />
+    </v-content>
+
+    <v-bottom-navigation app>
+      <!-- ボトムナビゲーション -->
+      <v-btn v-for="menu in menus" :key="menu.title" :to="menu.url">
+        <span>{{ menu.title }}</span>
+        <v-icon>{{ menu.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    drawer: false,
+    menus: [
+      { title: '説明', icon: 'mdi-web', url: '/' },
+      { title: '作る！', icon: 'mdi-home', url: '/maker' },
+      { title: 'みんなの', icon: 'mdi-heart', url: '/timeline' },
+      { title: 'これについて', icon: 'mdi-information-variant', url: '/about' }
+    ]
+  })
+}
+</script>
